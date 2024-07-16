b302e60ea9290d1e1dfe737c62570180a00fc02f
richiethie <richiethie@gmail.com>
2024-07-15 19:42:29 -0500

    add to Github

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true})
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully")
+})
+
+app.listen(PORT, () => {
+    console.log(`Express server listening on http://localhost:${PORT}`)
+})
