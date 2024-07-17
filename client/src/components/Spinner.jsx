fd99e96c75bfb1e7e080635c8477375363b85e15
richiethie <richiethie@gmail.com>
2024-07-16 22:57:33 -0500

    added backend to frontend functionality

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -5,3 +5,5 @@ const Spinner = () => {
         <div className="animate-ping w-16 h-16 m-8 rounded-full bg-sky-600"></div>
     )
 }
+
+export default Spinner
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import Spinner from '../components/Spinner'
 import { Link } from 'react-router-dom'
 import { AiOutlineEdit } from 'react-icons/ai'
-import { BsInfoCircle } from 'react-icons/bs'
+import { FaPlus } from "react-icons/fa";
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md'
 
 const Home = () => {
@@ -14,7 +14,8 @@ const Home = () => {
         axios
             .get('http://localhost:3001/products')
             .then((response) => {
-                setProducts(response.data.data)
+                console.log(response.data)
+                setProducts(response.data)
                 setLoading(false)
             })
             .catch((error) => {
@@ -30,6 +31,48 @@ const Home = () => {
                     <MdOutlineAddBox className='text-sky-800 test-4xl' />
                 </Link>
             </div> 
+            {loading ? (
+                <Spinner />
+            ) : (
+                <table className='w-full border-separate border-spacing'>
+                    <thead>
+                        <tr>
+                        <th className='border border-slate-600 rounded-md'>No.</th>
+                            <th className='border border-slate-600 rounded-md'>Name</th>
+                            <th className='border border-slate-600 rounded-md'>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {products.map((product, index) => (
+                            <tr key={product._id} className='h-8'>
+                                <td className='border border-slate-700 rounded-md text-center'>
+                                    {index + 1}
+                                </td>
+                                <td className='border border-slate-700 rounded-md text-center'>
+                                    {product.name}
+                                </td>
+                                <td className='border border-slate-700 rounded-md text-center'>
+                                    {product.price}
+                                </td>
+                                <td className='border border-slate-700 rounded-md text-center'>
+                                    <div className='flex justify-center gap-x-4'>
+                                        {/* change this to adding product to cart */}
+                                        <Link to='/products/add'>
+                                        <FaPlus className='text-2xl text-green-800' />
+                                        </Link>
+                                        <Link to={`/products/update/${product._id}`}>
+                                            <AiOutlineEdit className='text-2xl text-yellow-600' />
+                                        </Link>
+                                        <Link to={`/products/delete/${product._id}`}>
+                                            <MdOutlineDelete className='text-2xl text-red-600' />
+                                        </Link>
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
         </div>
     )
 }
